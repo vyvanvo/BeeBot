cd90a96dcaaa38f3a1f318fe1fea9b69204228ea
vyvanvo <yvvanvo@gmail.com>
2021-01-16 15:29:51 -0800

    beebot has commands

--- beebot.py
+++ beebot.py
@@ -1,12 +1,29 @@
 import discord
 from discord.ext import commands
+import random
 
-client = commands.Bot(command_prefix = "bee")
+client = commands.Bot(command_prefix = 'buzz ')
+token = "ODAwMTAwMzE1MTg5MzQ2MzE2.YANNfA.A3ThDWdomTzPlWMg_WEGhWnGHDw"
+
+#positive quotes
+
+#motivational quotes
+motivational_quotes = ['\"If you cannot do great things,do small things in a great way\" -*Napoleon Hill*']
 
 @client.event
 #when the bot is ready
 async def on_ready():
-    print("beebot is ready")
+    print('beebot is ready.')
 
-client.run("ODAwMTAwMzE1MTg5MzQ2MzE2.YANNfA.A3ThDWdomTzPlWMg_WEGhWnGHDw")
+@client.command()
+async def qotd(ctx):
+    await ctx.send('A smooth sea never make a skillful sailor.')
+
+@client.command(name='motivational')
+async def motivation(ctx):
+    m_quote = random.choice(motivational_quotes)
+    await ctx.send(m_quote)
+
+
+client.run(token)
 
