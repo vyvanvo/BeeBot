3a75ee66e9307c3d64295764b7ba7ded2c58fd3f
vyvanvo <yvvanvo@gmail.com>
2021-01-16 14:20:11 -0800

    runs beebot on discord

--- beebot.py
+++ beebot.py
@@ -1,1 +1,12 @@
-print("Hello World")
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = "bee")
+
+@client.event
+#when the bot is ready
+async def on_ready():
+    print("beebot is ready")
+
+client.run("ODAwMTAwMzE1MTg5MzQ2MzE2.YANNfA.A3ThDWdomTzPlWMg_WEGhWnGHDw")
+
